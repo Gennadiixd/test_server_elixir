a26688d04577910f8a68d2602b021652d3e86349
gennadii <gennadiixd@gmail.com>
2021-08-19 11:00:24 +0300

    return tuple

--- lib/controller/github_repos_controller.ex
+++ lib/controller/github_repos_controller.ex
@@ -1,7 +1,7 @@
 defmodule GitHubReposController do
   # def get_ids(repos) do
   # end
-    # plug(Plug.Parsers,
+  # plug(Plug.Parsers,
   #   parsers: [:json],
   #   pass: ["*/*", "application/json", "text/plain"],
   #   json_decoder: Poison
@@ -13,15 +13,21 @@ defmodule GitHubReposController do
   #   |> Map.take(@expected_fields)
   #   |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
   # end
+  def get_id(repo) do
+    Map.get(repo, "id")
+  end
+
+  def get_name(repo) do
+    Map.get(repo, "name")
+  end
 
   def get_repos(user) do
     response = HTTPoison.get!("https://api.github.com/users/#{user}/repos")
 
     response.body
     |> Poison.decode!()
-    |> Enum.map(fn repo -> Map.get(repo, "name") end)
+    |> Enum.map(fn repo -> {get_name(repo), get_id(repo)} end)
+    |> Enum.map(fn repo -> Tuple.to_list(repo) end)
     |> Poison.encode!()
-
-    # |> Map.take(~w(id))
   end
 end
